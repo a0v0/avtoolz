fdddb99f0e833cac76ec4ac68016b0aaa8543ee9
aV <9752193-a0v0@users.noreply.gitlab.com>
2022-11-08 01:39:46 +0530

    vxzzxv

--- src/utils/size-calc.ts
+++ src/utils/size-calc.ts
@@ -1,11 +1,10 @@
-import humanize from "humanize";
-
-export function getTotalSize(files: File[]): number {
-  return humanize.filesize(files.reduce((total, file) => total + file.size, 0));
+import { filesize } from "filesize";
+export function getTotalSize(files: File[]) {
+  return filesize(files.reduce((total, file) => total + file.size, 0));
 }
 
-export function getFileSizeFromDataUri(dataUri: string): number {
+export function getFileSizeFromDataUri(dataUri: string) {
   const base64 = dataUri.split(",")[1];
   const padding = base64.endsWith("=") ? (base64.endsWith("==") ? 2 : 1) : 0;
-  return humanize.filesize(base64.length * 0.75 - padding);
+  return filesize(base64.length * 0.75 - padding);
 }
