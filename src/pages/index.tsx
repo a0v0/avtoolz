31be416a3a94d7fb3bc82fc888a5189d5e3a6ac8
aV <9752193-a0v0@users.noreply.gitlab.com>
2022-11-17 00:07:04 +0530

    revert index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,9 @@ import { getRawFileFromRepo } from "@lib/github/raw";
 import { MetaProps } from "@lib/tools/meta";
 import { fetchDocsManifest, findRouteByPath, Route } from "@lib/tools/page";
 import { getSlug } from "@lib/tools/utils";
-import Markdown from "markdown-to-jsx";
+import manifest from "manifest.json";
 import { GetServerSideProps } from "next";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
 
@@ -36,7 +37,40 @@ const IndexPage: React.FC<Props> = ({ routes, currentRoute, content }) => {
       slug={router.route}
       tag={tag}
     >
-      <Markdown>{content}</Markdown>
+      <h1 style={{ textAlign: "center" }}>👋 Welcome netizens!</h1>
+      <Image
+        src="/mstile-310x310.png"
+        alt="aVToolz site logo"
+        title=""
+        width="10vh"
+        height="3vh"
+        layout="responsive"
+        objectFit="contain"
+      />
+      <p>
+        aVToolz is a handy website containg a collection of tools to perform
+        daily tasks. aVToolz offer tools for image conversion, file conversion,
+        text transformation and many more...
+      </p>
+      <br></br>
+      <p>
+        New tools are added to this site frequently so do not forget to give a
+        star on <a href={manifest.github_repo_url}>GitHub</a> and opt for
+        notifications whenever a new version comes out.
+      </p>
+      <br></br>
+      <p>
+        Choose your tools from the sidebar or if on mobile from navigation bar.
+      </p>
+      <br></br>
+      <h2>😘 Available Tools</h2>
+      <p>
+        Thanks to these awesome tools/libraries/resources that made aVToolz
+        possible:
+      </p>
+      <ul>
+        <li></li>
+      </ul>
     </ToolsLayout>
   );
 };
