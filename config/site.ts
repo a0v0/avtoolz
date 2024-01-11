e3fe79204a6e48aea1eb24ddf76f4ca759a94a43
aV <62926341+a0v0@users.noreply.github.comtask>
2024-01-11 11:12:46 +0530

    Update site configuration

--- config/site.ts
+++ config/site.ts
@@ -1,35 +1,34 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "NextUI - Beautiful, fast and modern React UI Library",
-  description: "Make beautiful websites regardless of your design experience.",
-  ogImage: "https://nextui.org/twitter-cards/nextui.jpeg",
+  name: "aVToolz • Modern, fast, and user-friendly tools for everyone.",
+  description: "Modern, fast, and user-friendly tools for everyone.",
+  ogImage: "https://avtoolz.com/twitter-cards/avtoolz.jpeg",
   author: "Anubhav Mahur",
-  email: "avm244@proton.me",
+  email: "",
   siteUrl: "https://avtoolz.com",
-  creator: "@getnextui",
+  creator: "@a0v0",
   openGraph: {
     type: "website",
     locale: "en_US",
-    url: "https://nextui.org",
-    siteName: "NextUI",
-    description: "Beautiful, fast and modern React UI Library",
+    url: "https://avtoolz.com",
+    siteName: "aVToolz",
+    description: "Modern, fast, and user-friendly tools for everyone.",
     images: [
       {
-        url: "https://nextui.org/twitter-cards/nextui.jpeg",
+        url: "https://avtoolz.com/twitter-cards/avtoolz.jpeg",
         width: 1200,
         height: 630,
-        alt: "NextUI",
+        alt: "aVToolz",
       },
     ],
   },
   twitter: {
     card: "summary_large_image",
-    title: "NextUI - Beautiful, fast and modern React UI Library",
-    description:
-      "Make beautiful websites regardless of your design experience.",
-    image: "https://nextui.org/twitter-cards/nextui.jpeg",
-    creator: "@getnextui",
+    title: "aVToolz • Modern, fast, and user-friendly tools for everyone.",
+    description: "Modern, fast, and user-friendly tools for everyone.",
+    image: "https://avtoolz.com/twitter-cards/avtoolz.jpeg",
+    creator: "@avtoolz",
   },
   links: {
     github: "https://github.com/a0v0/avtoolz",
