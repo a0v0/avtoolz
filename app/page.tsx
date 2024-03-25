5cb6c796fb4b5aa26e9f662c39db9d632841a84f
aV <62926341+a0v0@users.noreply.github.com>
2024-03-26 00:05:53 +0530

    Remove commented out code in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,7 @@ import BackgroundEffects from "@/components/background-effects";
 import HeroX from "@/components/hero";
 import {ToolsCards} from "@/components/tools";
 
-// async function getData() {
-//   try {
-//     const sponsors = await getAllSponsors();
-
-//     return {
-//       sponsors,
-//     };
-//   } catch (error) {
-//     throw new Error("Failed to fetch data");
-//   }
-// }
-
 export default async function Home() {
-  // const data = await getData();
-
   return (
     <main className="container mx-auto max-w-7xl px-6 flex-grow">
       <section className="flex flex-col items-center justify-center">
