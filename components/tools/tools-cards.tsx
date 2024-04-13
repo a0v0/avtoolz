0c79f1e288fec0451bdcc0249afde97d7e478e36
aV <62926341+a0v0@users.noreply.github.com>
2024-04-13 09:58:36 +0530

    feat: hide category headers if no tools exists

--- components/tools/tools-cards.tsx
+++ components/tools/tools-cards.tsx
@@ -8,38 +8,40 @@ export const ToolsCards = () => {
   return (
     <>
       <section id="all_tools" className={sectionWrapper({})}>
-        {manifest.routes.map((category) => (
-          <div key={category.key} className="flex flex-col gap-8">
-            <div>
-              <h1
-                className={title({
-                  size: "lg",
-                  color: category.color as
-                    | "violet"
-                    | "yellow"
-                    | "blue"
-                    | "cyan"
-                    | "green"
-                    | "pink"
-                    | "foreground"
-                    | undefined,
-                })}
-              >
-                # {category.title}
-              </h1>
+        {manifest.routes.map((category) =>
+          category.routes.length > 0 ? (
+            <div key={category.key} className="flex flex-col gap-8">
+              <div>
+                <h1
+                  className={title({
+                    size: "lg",
+                    color: category.color as
+                      | "violet"
+                      | "yellow"
+                      | "blue"
+                      | "cyan"
+                      | "green"
+                      | "pink"
+                      | "foreground"
+                      | undefined,
+                  })}
+                >
+                  # {category.title}
+                </h1>
+              </div>
+              <FeaturesGrid
+                classNames={{
+                  base: "lg:grid-cols-3",
+                  iconWrapper: "bg-transparent",
+                  header: "pt-2",
+                  body: "pt-0 pb-2",
+                }}
+                features={category.routes}
+              />
+              <Spacer y={10} />
             </div>
-            <FeaturesGrid
-              classNames={{
-                base: "lg:grid-cols-3",
-                iconWrapper: "bg-transparent",
-                header: "pt-2",
-                body: "pt-0 pb-2",
-              }}
-              features={category.routes}
-            />
-            <Spacer y={10} />
-          </div>
-        ))}
+          ) : null,
+        )}
       </section>
     </>
   );
--- config/config.tsx
+++ config/config.tsx
@@ -1,22 +1,21 @@
-import {OpenCollectiveIcon} from "@/components/icons";
 import {ToolCategory, ToolType} from "@/types/tool";
 import {Image} from "@nextui-org/react";
 import React from "react";
 
 export const Tools: ToolType[] = [
-  // {
-  //   key: "image-to-pdf",
-  //   title: "Image to PDF",
-  //   icon: <Image src="/icons/image-to-pdf.webp" width={35} />,
-  //   description: "Convert your images to PDF format effortlessly using our intuitive online tool.",
-  //   showFullDescription: true,
-  //   href: "/image-to-pdf",
-  //   isExternal: false,
-  //   keywords: "image to pdf, convert image to pdf, image converter, pdf converter",
-  //   category: [ToolCategory.PDF],
-  //   updated: false,
-  //   newPost: false,
-  // },
+  {
+    key: "image-to-pdf",
+    title: "Image to PDF",
+    icon: <Image src="/icons/image-to-pdf.webp" width={35} />,
+    description: "Convert your images to PDF format effortlessly using our intuitive online tool.",
+    showFullDescription: true,
+    href: "/image-to-pdf",
+    isExternal: false,
+    keywords: "image to pdf, convert image to pdf, image converter, pdf converter",
+    category: [ToolCategory.PDF],
+    updated: false,
+    newPost: false,
+  },
 
   {
     key: "merge-pdf",
@@ -29,21 +28,6 @@ export const Tools: ToolType[] = [
     keywords:
       "merge pdf, combine pdf, pdf merger, merge pdf documents, pdf merge online, merge pdf files, pdf merge tool",
     category: [ToolCategory.PDF],
-    updated: false,
-    newPost: false,
-  },
-
-  {
-    key: "compress-pdf",
-    title: "Compress JPG",
-    description: "Compress JPG images without losing quality.",
-    showFullDescription: true,
-    icon: <OpenCollectiveIcon fill="#FF4ECD" />,
-    href: "/tools/compress-jpg",
-    isExternal: false,
-    keywords:
-      "compress jpg, jpg compression, image compression, reduce jpg size, compress image, jpg optimizer, jpg compressor",
-    category: [ToolCategory.IMAGE],
     updated: false,
     newPost: false,
   },
