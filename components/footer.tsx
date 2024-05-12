e5bf06edda629bc91e32fd5209c1eb2307ec1d0d
aV <62926341+a0v0@users.noreply.github.com>
2024-05-12 20:29:55 +0530

    refactor: remove unused code in Footer component

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,18 +1,10 @@
 "use client";
 
-import {usePathname} from "next/navigation";
-
 import {Link} from "@nextui-org/react";
 import {SmilingFaceWithHeartsEmoji} from "./icons/fluentui-emoji";
 import {title} from "./primitives";
 
 export const Footer = () => {
-  const pathname = usePathname();
-
-  if (pathname.includes("/examples")) {
-    return null;
-  }
-
   return (
     <footer className="w-full flex gap-1 items-center justify-center py-3 text-current">
       Crafted with <SmilingFaceWithHeartsEmoji className="inline" size={20} /> on{" "}
