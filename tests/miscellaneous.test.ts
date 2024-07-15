12fc5bb744627f403756e4ffdc41534c3ce9e36f
aV <62926341+a0v0@users.noreply.github.com>
2024-07-15 13:30:12 +0530

    feat: Enable theme toggle functionality
    This commit enables the theme toggle functionality on the webpage. Users can now switch between light mode and dark mode by clicking on the theme toggle button. The code has been updated to ensure that the correct CSS classes are applied to the HTML element based on the selected theme.
    
    Fixes #123

--- tests/miscellaneous.test.ts
+++ tests/miscellaneous.test.ts
@@ -3,35 +3,40 @@ import { expect, test } from "@playwright/test";
 test.describe("theme toggle", () => {
   test("light mode working?", async ({ page }) => {
     await page.goto("/");
+    // await page
+    //   .locator("ul")
+    //   .filter({ hasText: "RoadmapReport Bugs Quick" })
+    //   .getByLabel("Switch to light mode")
+    //   .getByRole("link")
+    //   .click();
+    // page.getByRole("link", { name: "Switch to light mode" }).click();
+
     await page
       .locator("ul")
       .filter({ hasText: "RoadmapReport Bugs Quick" })
-      .getByLabel("Switch to light mode")
-      .getByRole("link")
+      .locator("#theme-toggle")
       .click();
-    await expect(page.locator("html")).toHaveAttribute("class", "light");
+    await expect(page.locator("html")).toHaveClass("enableSystem light");
     await page.reload();
-    await expect(page.locator("html")).toHaveAttribute("class", "light");
+    await expect(page.locator("html")).toHaveClass("light");
   });
 
   test("dark mode working?", async ({ page }) => {
     await page.goto("/");
-    // default theme is dark so first switch to light
     await page
       .locator("ul")
       .filter({ hasText: "RoadmapReport Bugs Quick" })
-      .getByLabel("Switch to light mode")
-      .getByRole("link")
+      .locator("#theme-toggle")
       .click();
     await page
       .locator("ul")
       .filter({ hasText: "RoadmapReport Bugs Quick" })
-      .getByLabel("Switch to dark mode")
-      .getByRole("link")
+      .locator("#theme-toggle")
       .click();
-    await expect(page.locator("html")).toHaveAttribute("class", "dark");
+    await expect(page.locator("html")).toHaveClass("enableSystem dark");
     await page.reload();
-    await expect(page.locator("html")).toHaveAttribute("class", "dark");
+
+    await expect(page.locator("html")).toHaveClass("dark");
   });
 });
 
