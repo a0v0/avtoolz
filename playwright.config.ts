a738a42ecb5812d01b6bc091a4c5dc7c98d6e108
aV <62926341+a0v0@users.noreply.github.com>
2024-07-18 10:49:32 +0530

    chore: Update CI workflow to use actions/upload-artifact@v4 and retain playwright-report for 30 days

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,11 +23,11 @@ export default defineConfig({
   // Fail the build on CI if you accidentally left test.only in the source code.
   forbidOnly: !!process.env.CI,
   // Retry on CI only
-  retries: process.env.CI ? 2 : 0,
+  // retries: process.env.CI ? 2 : 0,
   // Opt out of parallel tests on CI
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 1 : undefined,
   // Limit the number of failures on CI to save resources
-  maxFailures: process.env.CI ? 10 : undefined,
+  // maxFailures: process.env.CI ? 10 : undefined,
   // Reporter to use. See https://playwright.dev/docs/test-reporters
   reporter: process.env.CI ? "github" : "list",
 
@@ -55,17 +55,17 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-    ...(process.env.CI
-      ? [
-          {
-            name: "firefox",
-            use: { ...devices["Desktop Firefox"] },
-          },
-          {
-            name: "webkit",
-            use: { ...devices["Desktop Safari"] },
-          },
-        ]
-      : []),
+    // ...(process.env.CI
+    //   ? [
+    //       {
+    //         name: "firefox",
+    //         use: { ...devices["Desktop Firefox"] },
+    //       },
+    //       {
+    //         name: "webkit",
+    //         use: { ...devices["Desktop Safari"] },
+    //       },
+    //     ]
+    //   : []),
   ],
 });
